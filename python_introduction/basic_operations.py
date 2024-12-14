af85c1c4edb92f70292872d64f82386cb004a40a
steph-leo <leostephs89@gmail.com>
2024-12-15 01:15:06 +0300

    add updates

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+substruction = number1 - number2
+multiplication = number1 * number2
+print(addition)
+print(substruction)
+print(multiplication)
