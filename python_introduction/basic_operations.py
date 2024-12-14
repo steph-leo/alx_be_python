5f3da7123c9c6c342cda7c5eaf9888ac84859c73
steph-leo <leostephs89@gmail.com>
2024-12-15 01:21:09 +0300

    add updates

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,6 @@ number2 = 5
 addition = number1 + number2
 substruction = number1 - number2
 multiplication = number1 * number2
-print(addition)
-print(substruction)
-print(multiplication)
+print("Addition of 10 and 5 is",addition)
+print("Substraction of 10 and 5 is",substruction)
+print("Multiplication of 10 and 5 is",multiplication)
